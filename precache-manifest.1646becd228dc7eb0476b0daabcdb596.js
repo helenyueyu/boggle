ad41c2417e25c34d59fdfb7fd6b9455703f820c7
tokyosuite <helenyueyu@gmail.com>
2019-02-07 13:35:24 -0800

    Updates

---
+++ precache-manifest.1646becd228dc7eb0476b0daabcdb596.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e13e6bb079100f1d7ba3",
+    "url": "/boggle/static/css/main.5ec5d65c.chunk.css"
+  },
+  {
+    "revision": "e13e6bb079100f1d7ba3",
+    "url": "/boggle/static/js/main.e13e6bb0.chunk.js"
+  },
+  {
+    "revision": "64f3982b9779cc6418aa",
+    "url": "/boggle/static/js/1.64f3982b.chunk.js"
+  },
+  {
+    "revision": "cdc67b206a7a8d4e3594",
+    "url": "/boggle/static/js/runtime~main.cdc67b20.js"
+  },
+  {
+    "revision": "edb8d8ba31619e891bc50168f2d2512b",
+    "url": "/boggle/index.html"
+  }
+];

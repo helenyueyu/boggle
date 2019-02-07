81dd12d2e3bb88ba6198e1a8d477771a7b05b26a
tokyosuite <helenyueyu@gmail.com>
2019-02-07 13:31:26 -0800

    Still a bit stuck...need to figure out how to change the object in an immutable fashion. Maybe refactor the code a bit.

--- src/App.js
+++ src/App.js
@@ -45,7 +45,7 @@ class App extends Component {
   }
 
   render() {
-    // console.log(this.state.board)
+    console.log(this.state.board)
     // console.log(this.state.word)
     console.log(this.state.rows)
     console.log(this.state.cols)
@@ -57,6 +57,7 @@ class App extends Component {
       this.state.cols[this.state.cols.length-2]
     ))
     // console.log(this.state.board.filter(x => x.selected === true).map(x => x.tile))
+    const { board } = this.state
     return (
       <React.Fragment>
       <div className="App">
@@ -64,29 +65,15 @@ class App extends Component {
         <br/>
         <div className="board">
           <div className="row">
-            {this.state.board.filter((x,idx)=> idx >= 0 && idx <=3).map((x, idx) => <Tile key={idx}
+            {this.state.board.filter((x,idx)=> idx >= 0 && idx <=3).map((x, idx) =>
+
+            <Tile key={idx}
             tile={x.tile}
             row={x.row}
             col={x.col}
             selected={x.selected}
-            onClick={(e) => this.setState({
-              board: Object.assign(
-                [],
-                this.state.board, this.state.board[idx].selected = !this.state.board[idx].selected),
-
-              word: (this.state.board[idx].selected === true) ? this.state.word + e.target.innerHTML : this.state.word.slice(0, this.state.word.length - 1),
-
-              rows: (this.state.board[idx].selected === true) ? this.state.rows +  this.state.board[idx].row : this.state.rows.slice(0, this.state.rows.length - 1),
-
-              cols: (this.state.board[idx].selected === true) ? this.state.cols +  this.state.board[idx].col : this.state.rows.slice(0, this.state.rows.length - 1),
-
-              valid: (this.state.valid === false) ? false :
-              (this.state.rows.length <= 1) ? true : (this.distance(
-                this.state.rows[this.state.rows.length-1],
-                this.state.rows[this.state.rows.length-2],
-                this.state.cols[this.state.cols.length-1],
-                this.state.cols[this.state.cols.length-2]
-              ) === true) ? true : false
+            onClick={() => this.setState({
+              board: { ...this.state.board, selected: false }
             })}
             />)}
           </div>
@@ -100,6 +87,7 @@ class App extends Component {
               board: Object.assign(
                 [],
                 this.state.board, this.state.board[idx+4].selected = !this.state.board[idx+4].selected),
+
               word: (this.state.board[idx+4].selected === true) ? this.state.word + e.target.innerHTML : this.state.word.slice(0, this.state.word.length - 1),
               rows: (this.state.board[idx+4].selected === true) ? this.state.rows +  this.state.board[idx+4].row : this.state.rows.slice(0, this.state.rows.length - 1),
 
@@ -150,6 +138,7 @@ class App extends Component {
               board: Object.assign(
                 [],
                 this.state.board, this.state.board[idx+12].selected = !this.state.board[idx+12].selected),
+
               word: (this.state.board[idx+12].selected === true) ? this.state.word + e.target.innerHTML : this.state.word.slice(0, this.state.word.length - 1),
 
               rows: (this.state.board[idx+12].selected === true) ? this.state.rows +  this.state.board[idx+12].row : this.state.rows.slice(0, this.state.rows.length - 1),
@@ -166,7 +155,6 @@ class App extends Component {
             })}
             />)}
           </div>
-          // <div>{(this.state.valid === false) ? alert('Hello') : null}</div>
         </div>
       </div>
       </React.Fragment>
